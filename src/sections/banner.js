5c2df0e50f0a37061332d68f669bfcd4183cbea9
Bateristico <camilo.saa@gmail.com>
2022-05-15 22:05:46 +0200

    background shape image and banner

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -3,11 +3,26 @@ import { jsx } from 'theme-ui';
 import { Container, Box, Heading, Text, Image, Button } from 'theme-ui';
 import BannerImg from 'assets/banner-thumb.png';
 import ShapeLeft from 'assets/shape-left.png';
-import ShapeRight from 'assets/shape-right.png';
+import ShapeRight from 'assets/shape-right-2.png';
 
 export default function Banner() {
   return (
-    <h1>Banner</h1>
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <Box sx={styles.banner.contentBox}>
+          <Heading as="h1" variant="heroPrimary">
+            El mejor partner para alcanzar tus metas
+          </Heading>
+          <Text as="p" variant="heroSecondary">
+            Confía en nuestra experiencia y consigue tus objetivos
+          </Text>
+          <Button variant="primary">Explorar</Button>
+        </Box>
+        <Box sx={styles.banner.imageBox}>
+          <Image src={BannerImg} alt="banner" />
+        </Box>
+      </Container>
+    </section>
   );
 }
 
@@ -28,7 +43,7 @@ const styles = {
       backgroundImage: `url(${ShapeLeft})`,
       backgroundRepeat: `no-repeat`,
       backgroundPosition: 'bottom left',
-      backgroundSize: '36%',
+      backgroundSize: '36%'
     },
     '&::after': {
       position: 'absolute',
@@ -41,19 +56,19 @@ const styles = {
       backgroundImage: `url(${ShapeRight})`,
       backgroundRepeat: `no-repeat`,
       backgroundPosition: 'bottom right',
-      backgroundSize: '32%',
+      backgroundSize: '32%'
     },
     container: {
       minHeight: 'inherit',
       display: 'flex',
       flexDirection: 'column',
-      justifyContent: 'center',
+      justifyContent: 'center'
     },
     contentBox: {
       width: ['100%', '90%', '535px', null, '57%', '60%', '68%', '60%'],
       mx: 'auto',
       textAlign: 'center',
-      mb: ['40px', null, null, null, null, 7],
+      mb: ['40px', null, null, null, null, 7]
     },
     imageBox: {
       justifyContent: 'center',
@@ -62,8 +77,8 @@ const styles = {
       mb: [0, null, -6, null, null, '-40px', null, -3],
       img: {
         position: 'relative',
-        height: [245, 'auto'],
-      },
-    },
-  },
+        height: [245, 'auto']
+      }
+    }
+  }
 };
