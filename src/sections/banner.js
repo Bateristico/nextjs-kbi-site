45c72cde596d4963bfb1b13c49516c6803344802
Bateristico <camilo.saa@gmail.com>
2022-05-16 18:06:52 +0200

    change feature component and layout

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { Container, Box, Heading, Text, Image, Button } from 'theme-ui';
-import BannerImg from 'assets/banner-thumb.png';
+import BannerImg from 'assets/genexus-ide.png';
 import ShapeLeft from 'assets/shape-left.png';
 import ShapeRight from 'assets/shape-right-2.png';
 
