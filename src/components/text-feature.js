b43e1b798ffd441384e10bbeeaa02bf0611588f8
Bateristico <camilo.saa@gmail.com>
2022-05-17 21:22:53 +0200

    add quienes somos section

--- src/components/text-feature.js
+++ src/components/text-feature.js
@@ -1,15 +1,19 @@
 /** @jsx jsx */
 import { jsx, Box, Heading, Text, Button, Link } from 'theme-ui';
 
-export default function TextFeature({
-  subTitle,
-  title,
-  description,
-  btnName,
-  btnURL = '#',
-}) {
+export default function TextFeature({ subTitle, title, description, btnName, btnURL = '#' }) {
+  console.log(subTitle, title, description, btnName, btnURL);
   return (
-    <h1>TextFeature</h1>
+    <Box sx={styles.card}>
+      <Box sx={styles.wrapper}>
+        <Text as="p" sx={styles.wrapper.subTitle}>
+          {subTitle}
+        </Text>
+        <Heading as="h2" sx={styles.wrapper.title}>
+          {title}
+        </Heading>
+      </Box>
+    </Box>
   );
 }
 
@@ -20,8 +24,8 @@ const styles = {
     flexDirection: 'column',
     flexShrink: 0,
     a: {
-      m: ['0 auto', null, null, 0],
-    },
+      m: ['0 auto', null, null, 0]
+    }
   },
   wrapper: {
     width: '100%',
@@ -35,7 +39,7 @@ const styles = {
       fontWeight: '700',
       mb: [2, 3],
       lineHeight: 1.5,
-      letterSpacing: ['1.5px', null, '2px'],
+      letterSpacing: ['1.5px', null, '2px']
     },
     title: {
       fontSize: ['24px', null, '28px', '30px', '36px', '42px', null, '48px'],
@@ -43,14 +47,14 @@ const styles = {
       lineHeight: [1.3, null, null, null, 1.2],
       fontWeight: '700',
       letterSpacing: '-.5px',
-      mb: 5,
-    },
+      mb: 5
+    }
   },
   description: {
     fontSize: ['15px', 2, null, null, null, '17px', null, 3],
     fontWeight: 400,
     lineHeight: [1.85, null, null, 2, null, '2.2'],
     color: 'text_secondary',
-    mb: '30px',
-  },
+    mb: '30px'
+  }
 };
